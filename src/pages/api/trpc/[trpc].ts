184bf65a186075394ad90a45747ff9126a45e5d9
hongjiaherng <jiaherng2001@gmail.com>
2022-11-11 18:21:06 +0800

    Add tRPC

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,10 @@
+// import { appRouter } from '@/backend/router';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { appRouter } from '../../../backend/router';
+
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext: () => null,
+  });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
-      <div className='p-2' />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className='w-16 h-16 bg-red-200'></div>
-        <div className='p-8'>Vs</div>
-        <div className='w-16 h-16 bg-red-200'></div>
-        <div className=''></div>
-        <div className=''></div>
-        <div className=''></div>
-        <div className=''></div>
-      </div>
-    </div>
-  )
-}
+	const { data, isLoading } = trpc.useQuery(["hello", { text: "Jherng" }]);
 
-export default Home
+	if (isLoading) return <div>Loading...</div>;
+
+	if (data) return <div>{data.greeting}</div>;
+
+	return (
+		<div className="h-screen w-screen flex flex-col justify-center items-center">
+			<div className="text-2xl text-center">Which Pokémon is Rounder?</div>
+			<div className="p-2" />
+			<div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="p-8">Vs</div>
+				<div className="w-16 h-16 bg-red-200"></div>
+				<div className=""></div>
+				<div className=""></div>
+				<div className=""></div>
+				<div className=""></div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
