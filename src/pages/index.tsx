027241069036cd91a3300e640699e7d8a64f87d0
hongjiaherng <jiaherng2001@gmail.com>
2022-11-11 19:23:56 +0800

    add random fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { getOptionsForVote } from "../utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-	const { data, isLoading } = trpc.useQuery(["hello", { text: "Jherng" }]);
-
-	if (isLoading) return <div>Loading...</div>;
-
-	if (data) return <div>{data.greeting}</div>;
+	const [ids, setIds] = useState([0, 0]);
+	const [first, second] = ids
+	useEffect(() => {
+		setIds(getOptionsForVote());
+	}, []);
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center">
 			<div className="text-2xl text-center">Which Pokémon is Rounder?</div>
 			<div className="p-2" />
 			<div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-800">{first}</div>
 				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-200"></div>
+				<div className="w-16 h-16 bg-red-800">{second}</div>
 				<div className=""></div>
 				<div className=""></div>
 				<div className=""></div>
