8bc158479cf6f4f750e50c33d8ad6fb23c049d40
hongjiaherng <jiaherng2001@gmail.com>
2022-11-18 16:37:48 +0800

    Fix trpc link issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,18 @@ import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "../backend/router";
 import "../styles/globals.css";
 
+
+function getBaseUrl() {
+	if (typeof window !== "undefined") return "";
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+	return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+	// console.log(getBaseUrl());
 	return <Component {...pageProps} />;
 }
+
 
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
@@ -13,12 +22,13 @@ export default withTRPC<AppRouter>({
 		 * If you want to use SSR, you need to use the server's full URL
 		 * @link https://trpc.io/docs/ssr
 		 */
-		const url = process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}/api/trpc`
-			: "http://localhost:3000/api/trpc";
+		const url = `${getBaseUrl()}/api/trpc`;
+		// const url = process.env.VERCEL_URL
+		// 	? `https://${process.env.VERCEL_URL}/api/trpc`
+		// 	: "http://localhost:3000/api/trpc";
 
 		return {
-			url,
+			url
 			/**
 			 * @link https://react-query.tanstack.com/reference/QueryClient
 			 */
@@ -28,5 +38,5 @@ export default withTRPC<AppRouter>({
 	/**
 	 * @link https://trpc.io/docs/ssr
 	 */
-	ssr: true,
+	ssr: false,
 })(MyApp);
